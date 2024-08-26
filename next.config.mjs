40c58afbd9cd3ef9ce25523cc50320a60c3ce1b0
Bryan Borgeson <bryborge@gmail.com>
2024-08-26 10:16:20 -0700

    Enable export/output

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
+  output: 'export',
+  // TODO: Look into image optimization compatibility issue with npm run dev
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
